d3b81190af860c7b2aca3be7ad4e722568ec86ab
Nikita Marshalkin <marnikitta@gmail.com>
2016-10-24 16:29:48 +0300

    reversed done

---
+++ numanalysis/src/main/scala/ru/spbsu/math/marnikitta/homeworks/numanalysis/interpolation/application/Main.scala
@@ -0,0 +1,9 @@
+package ru.spbsu.math.marnikitta.homeworks.numanalysis.interpolation.application
+
+/**
+  * Created by marnikitta on 26.09.16.
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+  }
+}
---
+++ numanalysis/src/main/scala/ru/spbsu/math/marnikitta/homeworks/numanalysis/inversed/application/Main.scala
@@ -0,0 +1,60 @@
+package ru.spbsu.math.marnikitta.homeworks.numanalysis.inversed.application
+
+import ru.spbsu.math.marnikitta.homeworks.numanalysis.interpolation.domain.LagrangeInterpolator
+import ru.spbsu.math.marnikitta.homeworks.numanalysis.oddroots.domain.SegmentSearcher
+import ru.spbsu.math.marnikitta.homeworks.numanalysis.oddroots.domain.impl.BiSplitRoot
+
+/**
+  * Created by marnikitta on 24.10.16.
+  */
+object Main {
+  val f: Double => Double = x => Math.exp(x) - 2
+  val a = 1d
+  val b = 30d
+  val steps = 100
+  val power = 4
+  val eps = 1e-1
+  implicit val presision = 1e-8
+
+  val target = 54
+  val expected = Math.log(54 + 2)
+
+  def main(args: Array[String]): Unit = {
+    val step = (b - a) / steps
+    val args = Stream.iterate(a, steps)(a => a + 1).toIndexedSeq
+    val points = args.zip(args.map(f(_)))
+
+    println("All points:")
+    println(points)
+
+    println("\nTask 1\n")
+    taskOne(points)
+
+    println("\nTask 2\n")
+    taskTwo(points)
+  }
+
+  def taskTwo(points: Seq[(Double, Double)]): Unit = {
+    val chosen = points.sortBy(a => Math.abs(a._2 - target)).take(power + 1)
+
+    val qn = LagrangeInterpolator.interpolate(chosen)
+    val rootSearcher = new BiSplitRoot()
+
+    val segments = new SegmentSearcher(chosen.minBy(_._1)._1, chosen.maxBy(_._1)._1, eps).segments(x => qn(x) - target)
+    val roots = segments.map(s => rootSearcher.root(x => qn(x) - target, (x: Double) => x, s)).map(_.solution)
+
+    println("f^-1(target) = " + roots)
+    println("Residuals: " + roots.map(r => Math.abs(target - f(r))))
+  }
+
+  def taskOne(points: Seq[(Double, Double)]): Unit = {
+    println("Nearest points:")
+    val chosen = points.sortBy(a => Math.abs(a._2 - target)).take(power + 1)
+    println(chosen)
+
+    val qn = LagrangeInterpolator.interpolate(chosen.map(_.swap))
+    println()
+    println("f^-1(target) = " + qn(target))
+    println("Residual = " + Math.abs(target - f(qn(target))))
+  }
+}
